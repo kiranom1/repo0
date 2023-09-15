33f8755adec8540d4d6b69fcf93327c08db45dc6
unknown <kiran.om@gmail.com>
2023-09-15 23:54:00 +0100

    Calculating and printing total of three number(s)

---
+++ ThreeNumberTotal.c
@@ -0,0 +1,19 @@
+/* Calculating and printing total of three number(s) */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int number1=75;
+double number2=83.94; 
+float number3=347.68, total;
+
+int main() {
+	total = (number1+number2+number3);
+	printf("The sum-total of three numbers: %f \n", total);
+
+	return 0;
+}
+
+
