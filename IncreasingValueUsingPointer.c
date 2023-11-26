43ea87c345b76e2d7dbc89ec245684be56134ec5
unknown <kiran.om@gmail.com>
2023-11-26 17:10:18 +0000

    +'%A %d.%B.%Y'

---
+++ IncreasingValueUsingPointer.c
@@ -0,0 +1,20 @@
+/* Create a pointer to the local variable n called pointer_to_n, and use it to increase the value of n by one. */
+
+/* Program to increase value using pointer */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int n=99, *pointer_to_n=&n;
+
+int main() {
+	printf("The value of 'n': %d \n", n);
+
+	*pointer_to_n = (*pointer_to_n + 1);
+	//*pointer_to_n += 1;
+	
+	printf("The new value of 'n': %d \n", n);
+
+	return 0;
+}
